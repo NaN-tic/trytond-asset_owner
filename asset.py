dcdd73a57f5d2cbed830da35771b10efd1872a31
??ngel ??lvarez <angel@nan-tic.com>
2016-03-30 18:29:11 +0200

    asset_owner on date range

--- asset.py
+++ asset.py
@@ -1,6 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import PoolMeta, Pool
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.modules.asset.asset import AssetAssigmentMixin
 
@@ -8,7 +8,7 @@ __all__ = ['Asset', 'AssetOwner']
 __metaclass__ = PoolMeta
 
 
-class AssetOwner(ModelSQL, ModelView, AssetAssigmentMixin):
+class AssetOwner(AssetAssigmentMixin):
     'Asset Owner'
 
     __name__ = 'asset.owner'
@@ -23,3 +23,28 @@ class Asset:
 
     __name__ = 'asset'
     owners = fields.One2Many('asset.owner', 'asset', 'Owners')
+    current_owner = fields.Function(fields.Many2One('party.party',
+        'Current Owner'), 'get_current_owner')
+    current_owner_contact = fields.Function(fields.Many2One('party.party',
+        'Current Owner Contact'), 'get_current_owner')
+
+    @classmethod
+    def get_current_owner(cls, assets, names):
+        pool = Pool()
+        AssetOwner = pool.get('asset.owner')
+        assigments = cls.get_current_values(assets, AssetOwner)
+        result = {}
+        for name in names:
+            result[name] = dict((i.id, None) for i in assets)
+
+        print "***************************************************"
+        print assigments
+        for asset, assigment_id in assigments.iteritems():
+            print "asset", asset, assigment_id
+            if not assigment_id:
+                continue
+            assigment = AssetOwner(assigment_id)
+            result['current_owner'][asset] = assigment.owner.id
+            result['current_owner_contact'][asset] = assigment.contact and \
+                assigment.contact.id
+        return result
