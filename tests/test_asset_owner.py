77e900991f5bb5b657ecc878aa1046ac20e70d55
Raimon Esteve <raimonesteve@gmail.com>
2022-03-22 19:09:25 +0100

    Check rule only if _check_access is set and enforce companies rule issue4080

--- tests/test_asset_owner.py
+++ tests/test_asset_owner.py
@@ -5,9 +5,10 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown, doctest_checker
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class AssetOwnerTestCase(ModuleTestCase):
+class AssetOwnerTestCase(CompanyTestMixin, ModuleTestCase):
     'Asset Owner Test module'
     module = 'asset_owner'
 
